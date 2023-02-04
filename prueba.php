d1eba89941be54618399a9cb31df5929f0c69749
Juan Arrieta <juanfearrietaortiz@gmail.com>
2023-02-04 11:50:16 -0500

    Subida a git

---
+++ prueba.php
@@ -0,0 +1,12 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$correo = $_POST['correo'];
+$mensa = $_POST['mensaje'];
+
+echo $nombre;
+echo $apellido;
+echo $correo;
+echo $mensa;
+?>
